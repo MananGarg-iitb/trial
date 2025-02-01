159c704af1e0106ff935e1b2fe2d64fff0d59a13
MananGarg-iitb <144512698+MananGarg-iitb@users.noreply.github.com>
2025-02-01 15:18:08 +0530

    frustrated

--- src/Toplevel.v
+++ src/Toplevel.v
@@ -7,8 +7,9 @@ module Toplevel(clk,Bus_Out,Bus_In,rst,En,Rw, pause, regSelect, dispReg ,ALE, TT
 	wire Rw_Duplicate;
 	
 	input [1:0] regSelect;
-	output [7:0] dispReg;
-	
+	output [4:0] dispReg;
+
+	wire [2:0] dispReg_temp;
 	wire IReg_En, Mux_PC_Add_Sel, Mux_PC_In_Sel, PC_En, IAR_En, Acc_En, IReg_Buffer_Sel, PC_Buffer_Sel, IAR_Buffer_Sel, 
 	Acc_Buffer_Sel;
 	wire [7:0] Data_Bus_In;
@@ -24,7 +25,7 @@ module Toplevel(clk,Bus_Out,Bus_In,rst,En,Rw, pause, regSelect, dispReg ,ALE, TT
 	(.IReg_En(IReg_En), .Mux_PC_Add_Sel(Mux_PC_Add_Sel), .Mux_PC_In_Sel(Mux_PC_In_Sel), .PC_En(PC_En), .IAR_En(IAR_En), 
 	.Acc_En(Acc_En), .IReg_Buffer_Sel(IReg_Buffer_Sel), .PC_Buffer_Sel(PC_Buffer_Sel), .IAR_Buffer_Sel(IAR_Buffer_Sel), 
 	.Acc_Buffer_Sel(Acc_Buffer_Sel), .clk(clk), .rst(rst), .Mux_Acc_In_Sel(Mux_Acc_In_Sel), .ALU_Sel(ALU_Sel), .En(En), .Rw(Rw), 
-	.IReg_Data_Out(IReg_Data_Out), .PC_Data_Out(PC_Data_Out), .Acc_Data_Out(Acc_Data_Out), .regSelect(regSelect), .dispReg(dispReg), 
+	.IReg_Data_Out(IReg_Data_Out), .PC_Data_Out(PC_Data_Out), .Acc_Data_Out(Acc_Data_Out), .regSelect(regSelect), .dispReg({dispReg, dispReg_temp}), 
 	.pause(pause),.ALE(ALE));
 	
 	 Datapath path_of_data (.IReg_En(IReg_En), .Mux_PC_Add_Sel(Mux_PC_Add_Sel), .PC_En(PC_En), .IAR_En(IAR_En), .Acc_En(Acc_En),
